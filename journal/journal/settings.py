8078f6ac4ab2242099e6b7f368c6d82e9bf369e9
Baxrombek25 <ravashanovbaxrombek@gmail.com>
2025-06-11 18:13:10 +0500

    imtihon davomi 2

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.onrender.com','localhost']
+ALLOWED_HOSTS = [os.getenv('.onrender.com','localhost')]
 
 
 # Application definition
